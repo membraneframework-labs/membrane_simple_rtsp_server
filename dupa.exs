c54a23cda3d1e4c7fbfb74f5a8f96a62d16c04e2
noarkhh <jakub.pryc@gmail.com>
2024-09-25 18:00:36 +0200

    Add test

---
+++ dupa.exs
@@ -0,0 +1,6 @@
+Membrane.SimpleRTSPServer.start_link(
+  "/Users/noarkhh/Membrane/membrane_mp4_plugin/test/fixtures/isom/ref_aac.mp4",
+  port: 30001
+)
+
+Process.sleep(:infinity)
---
+++ test/integration_test.exs
@@ -0,0 +1,14 @@
+defmodule Membrane.SimpleRTSPServer.IntegrationTest do
+  use ExUnit.Case
+
+  @tag :tmp_dir
+  test "Received file is the same as the sent one", %{tmp_dir: tmp_dir} do
+    input_path = "test/fixtures/input.mp4"
+    output_path = Path.join(tmp_dir, "output.mp4")
+    Membrane.SimpleRTSPServer.start_link(input_path, port: 40001, realtime: true)
+    Process.sleep(50)
+    Boombox.run(input: "rtsp://localhost:40001/", output: output_path)
+
+    assert File.read!(input_path) == File.read!(output_path)
+  end
+end
